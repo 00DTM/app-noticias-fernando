46957ea59ec7eacfe30084a8c9ca3496a6795294
Dani Te <daniel.te.mar@gmail.com>
2023-05-18 19:11:41 +0200

    Noticias funcionan

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '6dbb02c70b7248ec81fa6433804ce281'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '6dbb02c70b7248ec81fa6433804ce281'
 };
 
 /*
