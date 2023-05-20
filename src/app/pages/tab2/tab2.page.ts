dbc1658d091b0392e15603a1b34719fa34af17b5
Dani Te <daniel.te.mar@gmail.com>
2023-05-20 12:42:45 +0200

    implementacion infinite -scroll

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -21,17 +21,34 @@ export class Tab2Page implements OnInit {
     this.newsService.getTopHeadlinesByCategory(this.selectedCategory)
       .subscribe(articles => {
         this.articles = [...articles];
-  });
-}
+      });
+  }
 
-  segmentChanged(event: any) {
-    this.selectedCategory = event.detail.value; // Asigna el valor seleccionado del segmento a la variable selectedCategory
+  segmentChanged(event: Event) {
+    this.selectedCategory = (event as CustomEvent).detail.value; // Asigna el valor seleccionado del segmento a la variable selectedCategory
     this.newsService.getTopHeadlinesByCategory(this.selectedCategory) // Llama al servicio para obtener los artículos principales de la categoría seleccionada
       .subscribe(articles => {
-        this.articles = [...articles]; 
-  });
+        this.articles = [...articles];
+      });
+  }
 
+  loadData(event: any) {
+    this.newsService.getTopHeadlinesByCategory(this.selectedCategory, true)
+      .subscribe(articles => {
+
+        if (articles.length === this.articles.length){
+          event.target.disabled = true;
+          return;
+        }
+
+        this.articles = articles;
+        
+        event.target.complete();
+      })
   }
 
 
+
+
 }
+
